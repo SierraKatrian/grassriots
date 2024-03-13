d094c72faee64f9522a97f6bb385668208edf4f2
Sierra Katrian <sierra@sierrakatrian.com>
2024-03-13 16:19:25 -0400

    Added api test

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('works');
+console.log('works again');
+
+async function logMovies() {
+    const response = await fetch("https://services.grassriots.io/");
+    const movies = await response.json();
+    console.log(movies);
+}
